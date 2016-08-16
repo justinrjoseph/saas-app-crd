27e96edd269de153877fe82010d59ced30d5e63a
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-16 22:39:30 +0000

    Set up foundation for confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
-  
   resources :members
   get 'home/index'
 
-   root :to => "home#index"
+ root :to => "home#index"
 
     
   # *MUST* come *BEFORE* devise's definitions (below)
   as :user do   
-    match '/user/confirmation' => 'milia/confirmations#update', :via => :put, :as => :update_user_confirmation
+    match '/user/confirmation' => 'confirmations#update', via: :put, :as => :update_user_confirmation
   end
 
   devise_for :users, :controllers => { 
     :registrations => "milia/registrations",
-    :confirmations => "milia/confirmations",
+    :confirmations => "confirmations",
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
-
-
 end
