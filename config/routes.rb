216290af6099c6a236ddb4d662e3db32ede2429d
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-19 23:29:25 +0000

    Implement payment for premium registration

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,17 @@ Rails.application.routes.draw do
   resources :members
   get 'home/index'
 
- root :to => "home#index"
-
+  root to: "home#index"
     
   # *MUST* come *BEFORE* devise's definitions (below)
   as :user do   
-    match '/user/confirmation' => 'confirmations#update', via: :put, :as => :update_user_confirmation
+    match '/user/confirmation' => 'confirmations#update', via: :put, as: :update_user_confirmation
   end
 
-  devise_for :users, :controllers => { 
-    :registrations => "milia/registrations",
-    :confirmations => "confirmations",
-    :sessions => "milia/sessions", 
-    :passwords => "milia/passwords", 
+  devise_for :users, controllers: { 
+    registrations: "registrations",
+    confirmations: "confirmations",
+    sessions: "milia/sessions", 
+    passwords: "milia/passwords", 
   }
 end
