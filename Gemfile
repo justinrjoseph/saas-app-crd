180c5b16e0daa2baa65ef04291d1e89ee006b72a
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-19 00:36:18 +0000

    Code upload of project artifacts to Amazon S3

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'milia'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
 gem 'bootstrap-datepicker-rails'
+gem 'aws-sdk'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,5 @@
 class Artifact < ActiveRecord::Base
+  before_save :upload_to_s3
   attr_accessor :upload
   belongs_to :project
   
@@ -9,6 +10,14 @@ class Artifact < ActiveRecord::Base
   validate :uploaded_file_size
   
   private
+  
+    def upload_to_s3
+      s3 = Aws::S3::Resource.new
+      tenant_name = Tenant.find(Thread.current[:tenant_id].name)
+      obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+      obj.upload_file(upload.path, acl: 'public-read')
+      self.key = obj.public_url
+    end
   
     def uploaded_file_size
       if upload
