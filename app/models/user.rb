c17a57f47d85e8a3d9e8adde80b66bdebe335e9e
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-20 01:30:11 +0000

    Add admin privilege and move navigation into partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   acts_as_universal_and_determines_account
   has_one :member, :dependent => :destroy
 
+  def is_admin?
+    is_admin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819013452) do
+ActiveRecord::Schema.define(version: 20160820011302) do
 
   create_table "artifacts", force: :cascade do |t|
     t.string   "name"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20160819013452) do
     t.integer  "tenant_id"
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
+    t.boolean  "is_admin",                     default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
